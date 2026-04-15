e87b64a90c8d570c0e5e6dd725700700d597ebed
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-15 00:14:49 -0400

    feat: add commonCommit constant for standardized commit message types

---
+++ src/constant/commit_constant.ts
@@ -0,0 +1,24 @@
+const commonCommit = [
+  'feat',
+  'fix',
+  'doc',
+  'remove',
+  'style',
+  'refactor',
+  'perf',
+  'test',
+  'ci',
+  'update',
+  'release',
+  'breaking',
+  'config',
+  'typo',
+  'asset',
+  'package',
+  'language',
+  'security',
+  'release',
+  'revert'
+];
+
+export { commonCommit };
