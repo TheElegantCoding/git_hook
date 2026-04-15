5c85be252313bd470f65fac28a0588423269e8b4
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-15 00:13:41 -0400

    feat: add iconFile constant for file type icons

---
+++ src/constant/icon_file.ts
@@ -0,0 +1,18 @@
+const iconFile = {
+  ts: '\ue8ca',
+  js: '\ue781',
+  tsx: '\ue7ba',
+  jsx: '\ue7ba',
+  html: '\ue736',
+  yaml: '\ue6a8',
+  yml: '\ue6a8',
+  tf: '\ue8bd',
+  tfvars: '\ue8bd',
+  file: '\udb80\ude14',
+  json: '\ueb0f',
+  astro: '\ue735',
+  css: '\ue749',
+  scss: '\ue749'
+};
+
+export { iconFile };
