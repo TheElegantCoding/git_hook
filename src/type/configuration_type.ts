f2a1574e6e47b30fc65c760d100cd6aed04d2113
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-15 20:37:17 -0400

    feat: add release configuration to support GitHub releases

--- src/type/configuration_type.ts
+++ src/type/configuration_type.ts
@@ -1,5 +1,9 @@
 type ConfigurationType = {
   lintStaged: Record<string, string>;
+  release: {
+    releaseToGithub: boolean;
+  };
+  packageManager: 'npm' | 'bun' | 'yarn' | 'pnpm';
   commitlint: {
     maxLength: number;
     allowedTypes: string[];
