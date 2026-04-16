8fe7a148743f124d81f1be0992f3ee0c3261ba63
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-15 21:07:20 -0400

    feat: update entry points in to include CLI and install scripts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: [
+    'src/index.ts',
+    'src/bin/cli.ts',
+    'src/util/install.ts'
+  ],
   format: ['esm', 'cjs'],
   dts: true,
   clean: true,
