061e44ec420ef87d8983751686c50585ac45c291
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-17 00:07:11 -0400

    fix: enhance logging for GitHub release creation process

--- src/module/git/github_release.ts
+++ src/module/git/github_release.ts
@@ -1,21 +1,23 @@
 import { getConfiguration } from '@src/util/file_configuration.js';
-import { logger } from '@src/util/logger.js';
+import { loggerLoader } from '@src/util/logger.js';
 import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 
 const githubCreateRelease = (version: string, releaseNotes: string) => {
   const configuration = getConfiguration();
 
-  if (configuration.release?.releaseToGithub === false) {
+  if (configuration.release.releaseToGithub === false) {
     return;
   }
 
   const temporaryNotesPath = './TEMP_RELEASE_NOTES.md';
 
-  logger.info(`Creating GitHub release for version ${version}...`);
+  const loader = loggerLoader(`Creating GitHub release for version ${version}...`);
+  loader.start();
   fs.writeFileSync(temporaryNotesPath, releaseNotes, 'utf8');
   execSync(`gh release create v${version} --title "Release ${version}" --notes-file "${temporaryNotesPath}"`, { stdio: 'ignore' });
   fs.unlinkSync(temporaryNotesPath);
+  loader.stop();
 };
 
 export { githubCreateRelease };
