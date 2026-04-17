9b08642b0f5a8edfe91ab7e2b1b6d4fbde8c3a20
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-17 16:06:33 -0400

    refactor: restructure CLI command handling and improve help menu formatting

--- src/module/cli/cli.ts
+++ src/module/cli/cli.ts
@@ -1,24 +1,40 @@
 #!/usr/bin/env node
+/* eslint-disable ts/switch-exhaustiveness-check */
 /* eslint-disable no-console */
 import { release } from '@src/module/release/release.js';
+import { init } from '@src/util/install.js';
 import { logger } from '@src/util/logger.js';
 
 const args = process.argv.slice(2);
+const command = args[0];
 
-if (args.includes('--help') || args.includes('-h')) {
-  console.log(`
+const helpMenu = `
     🚀 Gitlys Toolkit
     Usage: gitlys [command]
 
     Commands:
-      release    Analiza commits, genera changelog y sube a GitHub.
-      --help     Muestra esta ayuda.
-    `);
+      init          Configura Gitlys en el repo (Hooks y Config).
+      lint-staged   Ejecuta validaciones en archivos preparados.
+      release       Analiza commits, genera changelog y sube a GitHub.
+      --help        Muestra esta ayuda.
+`;
+
+if (args.includes('--help') || args.includes('-h')) {
+  console.log(helpMenu);
   process.exit(0);
 }
 
-if (args.includes('release')) {
-  release();
-} else {
-  logger.error('Comando no reconocido. Prueba con: gitlys release');
+switch (command) {
+  case 'init': {
+    init();
+    break;
+  }
+  case 'release': {
+    release();
+    break;
+  }
+  default: {
+    logger.error('Command not recognized. Use --help for available commands.');
+    process.exit(1);
+  }
 }
