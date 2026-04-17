39320278674227b269e0696809ad2f50381ffebc
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-16 23:24:37 -0400

    feat: add generateRelease function for automated release process

---
+++ src/module/release/generate_release.ts
@@ -0,0 +1,22 @@
+import { updateChangelog } from '@src/module/changelog/changelog_file.js';
+import { commitStagedVersionFiles } from '@src/module/git/git_commit.js';
+import { gitPushTag, gitPushHead } from '@src/module/git/git_push.js';
+import { createTag } from '@src/module/git/git_tag.js';
+import { githubCreateRelease } from '@src/module/git/github_release.js';
+import { updatePackageJson } from '@src/module/release/version.js';
+import { logger } from '@src/util/logger.js';
+
+import type { CommitType } from '@src/type/commit_type.js';
+
+const generateRelease = (nextVersion: string, releaseNotes: string, commits: CommitType[]) => {
+  updatePackageJson(nextVersion);
+  updateChangelog(nextVersion, commits);
+  commitStagedVersionFiles(nextVersion);
+  createTag(nextVersion);
+  gitPushHead();
+  gitPushTag(nextVersion);
+  githubCreateRelease(nextVersion, releaseNotes);
+  logger.success(`Release process completed successfully! New version: ${nextVersion}`);
+};
+
+export { generateRelease };
