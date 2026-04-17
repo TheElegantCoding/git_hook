7e97cf99da877efc2a97fcd03d2883eae64fd74a
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-16 23:21:36 -0400

    feat: implement release management functionality

---
+++ src/module/release/release.ts
@@ -0,0 +1,24 @@
+import { getStagedCommit } from '@src/module/git/git_commit.js';
+import { isWorkingDirectoryClean } from '@src/module/git/working_directory.js';
+import { generateRelease } from '@src/module/release/generate_release.js';
+import { generateReleaseNotes } from '@src/module/release/release_notes.js';
+import { getNextVersion, getCurrentVersion } from '@src/module/release/version.js';
+import { logger } from '@src/util/logger.js';
+
+const release = () => {
+  try {
+    logger.info('Releasing new version');
+    isWorkingDirectoryClean();
+    const currentVersion = getCurrentVersion();
+    const commits = getStagedCommit(currentVersion);
+    const nextVersion = getNextVersion(currentVersion, commits.map((commit) => { return commit.message; }) as string[]);
+    const releaseNotes = generateReleaseNotes(nextVersion, commits);
+
+    generateRelease(nextVersion, releaseNotes, commits);
+  } catch (error) {
+    logger.error(`Error during release process: ${(error as Error).message}`);
+    process.exit(1);
+  }
+};
+
+export { release };
