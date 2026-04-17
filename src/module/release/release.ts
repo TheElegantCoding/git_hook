25616402d2997171c14a2b43493a7deb1f2321d2
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-17 17:23:30 -0400

    fix: update release function to use async version selection

--- src/module/release/release.ts
+++ src/module/release/release.ts
@@ -1,18 +1,18 @@
-/* eslint-disable style/max-len */
+import { selectVersion } from '@src/module/cli/select_version.js';
 import { getStagedCommit } from '@src/module/git/git_commit.js';
 import { isWorkingDirectoryClean } from '@src/module/git/working_directory.js';
 import { generateRelease } from '@src/module/release/generate_release.js';
 import { generateReleaseNotes } from '@src/module/release/release_notes.js';
-import { getNextVersion, getCurrentVersion } from '@src/module/release/version.js';
+import { getCurrentVersion } from '@src/module/release/version.js';
 import { logger } from '@src/util/logger.js';
 
-const release = (bump?: string) => {
+const release = async () => {
   try {
     logger.info('Releasing new version');
     isWorkingDirectoryClean();
     const currentVersion = getCurrentVersion();
     const commits = getStagedCommit(currentVersion);
-    const nextVersion = getNextVersion(currentVersion, commits.map((commit) => { return commit.message; }) as string[], bump);
+    const nextVersion = await selectVersion();
     const releaseNotes = generateReleaseNotes(nextVersion, commits);
 
     generateRelease(nextVersion, releaseNotes, commits);
