96b49bf1e58e59346c6753a09b930ab1f76e2feb
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-16 23:07:26 -0400

    feat: add changelog constant for project documentation

---
+++ src/constant/changelog.ts
@@ -0,0 +1,21 @@
+const changelogConstant = {
+  template: `# Changelog
+
+All notable changes to this project will be documented in this file.
+
+### Versioning rules
+
+- **MAJOR**: when you make incompatible API changes or big changes in the project.
+- **MINOR**: when you add additional functionality
+- **PATCH**: with small changes in the project.
+
+One version have a structure like this (semantic version) - (version) - (date)
+
+The list of changes have the description - commit - author.
+
+---
+
+## Released`
+};
+
+export { changelogConstant };
