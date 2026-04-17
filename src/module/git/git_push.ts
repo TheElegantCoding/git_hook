0639ab976bd94e8ea0c2a538fad6d49f754b5f5c
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-17 17:40:18 -0400

    feat: add loading indicators for git push operations

--- src/module/git/git_push.ts
+++ src/module/git/git_push.ts
@@ -1,11 +1,18 @@
+import { loggerLoader } from '@src/util/logger.js';
 import { execSync } from 'node:child_process';
 
 const gitPushTag = (version: string): void => {
+  const loader = loggerLoader('Pushing tag to remote repository...');
+  loader.start();
   execSync(`git push origin v${version}`, { stdio: 'ignore' });
+  loader.stop();
 };
 
 const gitPushHead = (): void => {
+  const loader = loggerLoader('Pushing changes to remote repository...');
+  loader.start();
   execSync('git push origin HEAD', { stdio: 'ignore' });
+  loader.stop();
 };
 
 export { gitPushTag, gitPushHead };
