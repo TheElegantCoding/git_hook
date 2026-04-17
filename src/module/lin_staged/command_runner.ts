4a55fd767f77dede6df6a8a171e6cf216b27a826
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-16 23:26:54 -0400

    feat: implement runLintCommand function for executing specified files

---
+++ src/module/lin_staged/command_runner.ts
@@ -0,0 +1,21 @@
+import { stageFiles } from '@src/module/git/git_staged.js';
+import { fileReport } from '@src/util/file_report.js';
+import { loggerLoader } from '@src/util/logger.js';
+import { execSync } from 'node:child_process';
+
+const runLintCommand = (command: string, pattern: string, files: string[]): void => {
+  const spinner = loggerLoader(`Linting: "${command}" on pattern: "${pattern}"`);
+
+  try {
+    spinner.start();
+    execSync(`${command} ${files.join(' ')}`, { stdio: 'pipe' });
+    stageFiles(files);
+    spinner.stop();
+    fileReport(files);
+  } catch (error) {
+    spinner.stop();
+    throw error;
+  }
+};
+
+export { runLintCommand };
