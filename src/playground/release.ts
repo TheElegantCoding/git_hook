f8b3bd3926161f6c3eb06086ea08a9a8a877eb7e
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-16 23:11:28 -0400

    fix: update import path for release module

--- src/playground/release.ts
+++ src/playground/release.ts
@@ -1,7 +1,3 @@
-import { release } from '@src/module/release.js';
-import { execSync } from 'node:child_process';
+import { release } from '@src/module/release/release.js';
 
 release();
-
-execSync('git reset --soft HEAD~1');
-execSync('git tag -d v1.0.0');
