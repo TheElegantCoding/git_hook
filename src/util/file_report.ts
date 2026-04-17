024fd4832a3ca1f6dcbe7132b5ed661e4342ed1d
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-16 23:04:53 -0400

    feat: implement fileReport utility for processing file inputs

---
+++ src/util/file_report.ts
@@ -0,0 +1,21 @@
+import { reportLogger } from '@src/util/file_logger.js';
+import { getLongerName } from '@src/util/longer_name.js';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const fileReport = (files: string[]): void => {
+  const longerName = getLongerName(files);
+
+  files.forEach((file) => {
+    const absolutePath = path.isAbsolute(file)
+      ? file
+      : path.join(process.cwd(), file);
+    if (fs.existsSync(absolutePath)) {
+      const buffer = fs.readFileSync(absolutePath);
+      const fileObject = new File([buffer], path.basename(file));
+      reportLogger.file(fileObject, longerName);
+    }
+  });
+};
+
+export { fileReport };
