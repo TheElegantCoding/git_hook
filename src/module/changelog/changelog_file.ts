c211fc129ca3434a66486ab7ec398910005665ca
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-16 23:22:23 -0400

    feat: implement changelog management functions

---
+++ src/module/changelog/changelog_file.ts
@@ -0,0 +1,68 @@
+import { changelogConstant } from '@src/constant/changelog.js';
+import { getRepoUrl } from '@src/module/git/git_repo_url.js';
+import { getConfiguration } from '@src/util/file_configuration.js';
+import { logger } from '@src/util/logger.js';
+import fs from 'node:fs';
+import path from 'node:path';
+
+import type { CommitType } from '@src/type/commit_type.js';
+
+const createChangelogFile = () => {
+  const configuration = getConfiguration();
+  const fullPath = path.join(process.cwd(), configuration.changelog?.changelogPath ?? 'CHANGELOG.md');
+
+  if (!configuration.changelog?.generateChangelog) {
+    return;
+  }
+
+  if (!fs.existsSync(fullPath)) {
+    logger.info('Generating changelog...');
+    fs.writeFileSync(fullPath, changelogConstant.template);
+  }
+};
+
+const checkVersionExists = (version: string, fullPath: string): boolean => {
+  const existingContent = fs.readFileSync(fullPath, 'utf8');
+  const versionHeader = `## 🚀 Version [${version}]`;
+
+  if (existingContent.includes(versionHeader)) {
+    return true;
+  }
+  return false;
+};
+
+const createEntry = (version: string, fullPath: string, commits: CommitType[]): string => {
+  const date = new Date().toISOString().split('T')[0];
+  const existingContent = fs.readFileSync(fullPath, 'utf8');
+  const github = 'https://github.com';
+  const gitUrl = getRepoUrl();
+
+  const changes = commits.map((commit) => {
+    const { message, hash, author } = commit;
+    return `- ${message} [[${hash}](${gitUrl}/commit/${hash})] by [@${author}](${github}/${author})`;
+  });
+
+  const newEntry = `## 🚀 Version [${version}] - ${date}\n\n${changes.join('\n')}`;
+
+  const updatedContent = existingContent.replace('## Released', `## Released\n\n${newEntry}`);
+  return updatedContent;
+};
+
+const updateChangelog = (version: string, commits: CommitType[]) => {
+  const configuration = getConfiguration();
+  const fullPath = path.join(process.cwd(), configuration.changelog?.changelogPath ?? 'CHANGELOG.md');
+
+  if (!fs.existsSync(fullPath)) {
+    return;
+  }
+
+  if (checkVersionExists(version, fullPath)) {
+    return;
+  }
+
+  logger.info(`Updating changelog for version ${version} with ${commits.length} commits...`);
+  const updatedContent = createEntry(version, fullPath, commits);
+  fs.writeFileSync(fullPath, updatedContent);
+};
+
+export { updateChangelog, checkVersionExists, createChangelogFile };
