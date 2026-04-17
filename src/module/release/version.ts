5ce0f22323566ec88520dbdd1fe71f9ced492a93
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-17 17:23:17 -0400

    refactor: remove unused versioning functions from release module

--- src/module/release/version.ts
+++ src/module/release/version.ts
@@ -14,72 +14,6 @@ const updatePackageJson = (newVersion: string) => {
   fs.writeFileSync(packagePath, `${JSON.stringify(packageContent, null, 2)}\n`);
 };
 
-const updateVersion = (
-  currentVersion: string,
-  featCounter: number,
-  hasBreaking: boolean,
-  hasFix: boolean
-): string => {
-  let [
-    major,
-    minor,
-    patch
-  ] = currentVersion.split('.').map(Number) as [number, number, number];
-
-  if (hasBreaking || featCounter >= 10) {
-    major++;
-    minor = 0;
-    patch = 0;
-  } else if (featCounter > 0) {
-    minor++;
-    patch = 0;
-  } else if (hasFix) {
-    patch++;
-  }
-
-  return `${major}.${minor}.${patch}`;
-};
-
-const getNextVersion = (currentVersion: string, commits: string[], bump?: string): string => {
-  const featCounter = commits.filter((content) => { return content.toLowerCase().includes('feat:'); }).length;
-  const hasBreaking = commits.some((content) => { return content.includes('breaking'); });
-  const hasFix = commits.some((content) => { return content.toLowerCase().includes('fix:'); });
-
-  if (bump === 'major') {
-    return updateVersion(
-      currentVersion,
-      featCounter,
-      true,
-      hasFix
-    );
-  }
-
-  if (bump === 'minor') {
-    return updateVersion(
-      currentVersion,
-      10,
-      hasBreaking,
-      hasFix
-    );
-  }
-
-  if (bump === 'patch') {
-    return updateVersion(
-      currentVersion,
-      featCounter,
-      hasBreaking,
-      true
-    );
-  }
-
-  return updateVersion(
-    currentVersion,
-    featCounter,
-    hasBreaking,
-    hasFix
-  );
-};
-
 const getCurrentVersion = (logInfo = true): string => {
   const packagePath = path.join(process.cwd(), 'package.json');
 
@@ -94,4 +28,4 @@ const getCurrentVersion = (logInfo = true): string => {
   return packageContent.version;
 };
 
-export { getNextVersion, getCurrentVersion, updatePackageJson };
+export { getCurrentVersion, updatePackageJson };
