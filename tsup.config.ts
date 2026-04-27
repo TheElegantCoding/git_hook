c6d09a0f0d0849eeee02b190368b11ec45317ff4
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-26 23:00:55 -0400

    feat: add pre_commit_task and pre_push_task entries to build configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   entry: {
     index: 'src/index.ts',
     cli: 'src/module/cli/cli.ts',
-    lint_staged: 'src/lint_staged.ts'
+    pre_commit_task: 'src/pre_commit_task.ts',
+    pre_push_task: 'src/pre_push_task.ts'
   },
   format: ['esm'],
   dts: true,
