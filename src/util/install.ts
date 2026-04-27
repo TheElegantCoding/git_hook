c352aee3754317971375d303aa35222de13b5536
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-26 22:54:47 -0400

    feat: refactor git hook installation to include pre-push task and improve structure

--- src/util/install.ts
+++ src/util/install.ts
@@ -3,6 +3,8 @@ import { logger } from '@src/util/logger.js';
 import fs from 'node:fs';
 import path from 'node:path';
 
+import type { ConfigurationType } from '@src/type/configuration_type.js';
+
 const checkGitDirectory = (directory: string): boolean => {
   const gitPath = path.join(directory, '.git');
 
@@ -19,38 +21,55 @@ const checkGitDirectory = (directory: string): boolean => {
   return true;
 };
 
-const installCommand = (gitHooksDirectory: string) => {
-  const commitMessageHook = path.join(gitHooksDirectory, 'commit-msg');
-  const precommitHook = path.join(gitHooksDirectory, 'pre-commit');
-  const isProduction = import.meta.url.includes('node_modules');
-  const configuration = getConfiguration();
+const createHookContent = (configuration: ConfigurationType, isProduction: boolean) => {
   const install = isProduction ? 'node ./node_modules/gitlys/dist/index.js "$1"' : `${configuration.packageManager} src/index.ts "$1"`;
-  const lintStagedCommand = isProduction ? 'node ./node_modules/gitlys/dist/lint_staged.js "$1"' : `${configuration.packageManager} src/lint_staged.ts`;
+  const preCommitTaskCommand = isProduction ? 'node ./node_modules/gitlys/dist/pre_commit_task.js "$1"' : `${configuration.packageManager} src/pre_commit_task.ts`;
+  const prePushTaskCommand = isProduction ? 'node ./node_modules/gitlys/dist/pre_push_task.js "$1"' : `${configuration.packageManager} src/pre_push_task.ts`;
 
   const hookContent = `#!/bin/bash\n\n${install}`;
-  const preCommitContent = `#!/bin/bash\n\n${lintStagedCommand}`;
+  const preCommitContent = `#!/bin/bash\n\n${preCommitTaskCommand}`;
+  const prePushContent = `#!/bin/bash\n\n${prePushTaskCommand}`;
+
+  return { hookContent, preCommitContent, prePushContent };
+};
+
+const installCommand = (gitHooksDirectory: string) => {
+  const commitMessageHook = path.join(gitHooksDirectory, 'commit-msg');
+  const preCommitHook = path.join(gitHooksDirectory, 'pre-commit');
+  const prePushHook = path.join(gitHooksDirectory, 'pre-push');
+  const isProduction = import.meta.url.includes('node_modules');
+  const configuration = getConfiguration();
+
+  const { hookContent, preCommitContent, prePushContent } = createHookContent(configuration, isProduction);
 
   return {
     commitMessageHook,
-    precommitHook,
+    preCommitHook,
+    prePushHook,
     hookContent,
-    preCommitContent
+    preCommitContent,
+    prePushContent
   };
 };
 
 const prepare = ({
   commitMessageHook,
-  precommitHook,
+  preCommitHook,
+  prePushHook,
   hookContent,
-  preCommitContent
+  preCommitContent,
+  prePushContent
 }: ReturnType<typeof installCommand>) => {
   fs.writeFileSync(commitMessageHook, hookContent);
   fs.chmodSync(commitMessageHook, '755');
 
-  fs.writeFileSync(precommitHook, preCommitContent);
-  fs.chmodSync(precommitHook, '755');
+  fs.writeFileSync(preCommitHook, preCommitContent);
+  fs.chmodSync(preCommitHook, '755');
 
-  logger.setup('Commit-msg and pre-commit hooks installed successfully.');
+  fs.writeFileSync(prePushHook, prePushContent);
+  fs.chmodSync(prePushHook, '755');
+
+  logger.setup('Git hooks installed successfully!');
 };
 
 const init = () => {
@@ -63,17 +82,21 @@ const init = () => {
 
   const {
     commitMessageHook,
-    precommitHook,
     hookContent,
-    preCommitContent
+    preCommitHook,
+    prePushHook,
+    preCommitContent,
+    prePushContent
   } = installCommand(gitHooksDirectory);
 
   try {
     prepare({
       commitMessageHook,
-      precommitHook,
+      preCommitHook,
+      prePushHook,
       hookContent,
-      preCommitContent
+      preCommitContent,
+      prePushContent
     });
   } catch (error) {
     logger.error(`Error installing Gitlys hook: ${(error as Error).message}`);
