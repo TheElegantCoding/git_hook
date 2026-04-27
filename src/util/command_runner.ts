6ff23c7e514f227543374eba7288dfe9e18a2994
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-26 23:25:12 -0400

    refactor: simplify runCommand by removing spinner and error handling

--- src/util/command_runner.ts
+++ src/util/command_runner.ts
@@ -1,28 +1,20 @@
 import { stageFiles } from '@src/module/git/git_staged.js';
 import { fileReport } from '@src/util/file_report.js';
-import { loggerLoader } from '@src/util/logger.js';
+import { logger } from '@src/util/logger.js';
 import { execSync } from 'node:child_process';
 
 const runCommand = (command: string, files?: string[]): void => {
-  const spinner = loggerLoader(`Running: "${command}"`);
   const filesToPass = command.includes('tsc') ? '' : files?.map((entry) => { return `"${entry}"`; }).join(' ') ?? '';
 
-  try {
-    spinner.start();
-    execSync(`${command} ${filesToPass}`, { stdio: 'inherit' });
+  logger.info(`Running: "${command}"...`);
+  execSync(`${command} ${filesToPass}`, { stdio: 'inherit' });
 
-    if (files && files.length > 0) {
-      stageFiles(files);
-    }
+  if (files && files.length > 0) {
+    stageFiles(files);
+  }
 
-    spinner.stop();
-
-    if (files && files.length > 0) {
-      fileReport(files);
-    }
-  } catch (error) {
-    spinner.stop();
-    throw error;
+  if (files && files.length > 0) {
+    fileReport(files);
   }
 };
 
