5111417a2607ce17a9ba941bca973d0e579b8973
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-28 19:02:16 -0400

    fix: enhance loggerLoader with styled success and final messages

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -8,11 +8,16 @@ import {
 const logger = new LogManager();
 
 const loggerLoader = (message: string) => {
-  const badge = `${colorAnsi.bgBlue} ${loggerIcon.info} INFO ${colorAnsi.reset}`;
-  const loaderMessage = loggerStyle.ansi(message, { color: colorAnsi.blue });
+  const badge = loggerStyle.ansi(` ${loggerIcon.info} INFO `, { color: colorAnsi.blue, bg: colorAnsi.bgBlue, bold: true });
+  const successBadge = loggerStyle.ansi(` ${loggerIcon.check} SUCCESS `, { color: colorAnsi.green, bg: colorAnsi.bgGreen, bold: true });
+  const messageLoader = loggerStyle.ansi(message, { bold: true });
+  const finalMessage = `${loggerStyle.ansi(message, { bold: true })} ${loggerStyle.ansi(loggerIcon.check, { color: colorAnsi.green, bold: true })}`;
+
   const loaderInstance = logger.loader({
-    message: `${badge} ${loaderMessage}`,
-    showTimestamp: true
+    message: `${badge} ${messageLoader}`,
+    showTimestamp: true,
+    type: 'aesthetic',
+    finalMessage: `${successBadge} ${finalMessage}`
   });
 
   return loaderInstance;
