ab85d1ea6f8476a9586fad40a79f0f8e18e6cdd1
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-30 12:05:57 -0400

    fix: update getPushFiles to retrieve modified files between commits

--- src/module/git/git_push.ts
+++ src/module/git/git_push.ts
@@ -2,12 +2,20 @@ import { loggerLoader } from '@src/util/logger.js';
 import { execSync } from 'node:child_process';
 
 const getPushFiles = (): string[] => {
-  const output = execSync('git diff --name-only HEAD origin/HEAD')
+  const output = execSync('git log --format="%h" HEAD --not --remotes')
     .toString()
     .trim()
     .split('\n')
     .filter(Boolean);
-  return output;
+  const firstCommit = output[0] ?? '';
+  const lastCommit = output.at(-1) ?? '';
+  const outputLoader = execSync(`git diff --name-only ${firstCommit} ${lastCommit}`)
+    .toString()
+    .trim()
+    .split('\n')
+    .filter(Boolean);
+
+  return outputLoader;
 };
 
 const gitPushTag = (version: string): void => {
